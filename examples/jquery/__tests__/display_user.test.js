9601c8218c306e10ae28379d0fa15463356ffa1b
eirblaze <eirblaze@live.jp>
2020-04-06 18:01:35 +0900

    本物とモックが混じって認識されないように、メモを残す。

--- examples/jquery/__tests__/display_user.test.js
+++ examples/jquery/__tests__/display_user.test.js
@@ -23,7 +23,19 @@ it('displays a user after a click', () => {
   // Tell the fetchCurrentUser mock function to automatically invoke
   // its callback with some data
   // cb == callback
-  // .mockImplementation(fn) Functionのfnの返り値の模倣を作る。これで、モックになる。
+  // .mockImplementation(fn) 引数に入ったコールバックFunctionのfnの返り値の模倣を作る。これで、モックになる。
+  //
+  // $('#button').click(); が実行された後、fetchCurrentUserが起動する。その際に、モックで指示した変数が自動代入されている。
+  //
+  // 模倣元は success: user => callback(parseJSON(user)) 。同名の変数「user」に注意。
+  // fetchCurrentUser.js での user :string は、ajaxが返してきた文字列。parseJSON(user) によって加工される。
+  // displayUser.js での無名関数の引数 user :JSON には、上記のパースされたJSONが代入されている。
+  //
+  // .mockImplementationでは、上記のfetchCurrentUser.js での動きを、模倣に書き換える。
+  // $('#button').click(); が実行されると、本物ではなく、モックのfetchCurrentUserが実行される。
+  // cbには、本物と同じく、displayUser.js 側の無名関数が代入される。
+  // モックでは、success のときに実行する、を、無条件で実行。
+  // displayUser.js 側の無名関数 の引数を、ajaxからの文字列パース結果ではなく、固定値としている。
   fetchCurrentUser.mockImplementation(cb => {
     cb({
       fullName: 'Johnny Cash',
